aaf020030b7277fa3cea4b524e3ae0040c04996c
GCancro1 <george.cancro@yahoo.com>
2026-01-30 09:03:51 -0500

    oops

--- lua/setuplazy.lua
+++ lua/setuplazy.lua
@@ -1,5 +1,4 @@
 -- [[ Install `lazy.nvim` plugin manager ]]
-mes_out = p.read_from(offset, max_messages)
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
